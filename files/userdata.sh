08321b60cdc9750218536aa5015f093a82baffb3
Hector Vido <hector_vido@yahoo.com.br>
2020-04-29 17:36:27 -0300

    Adicionado proxy ao puppet.conf

--- files/userdata.sh
+++ files/userdata.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Squid
+echo 'export http_proxy=http://10.142.0.43:3128' >> /etc/profile
+echo 'export https_proxy=http://10.142.0.43:3128' >> /etc/profile
+. /etc/profile
+
 apt-get install -y wget
 
 wget https://apt.puppetlabs.com/puppet6-release-buster.deb
@@ -9,3 +14,11 @@ echo '10.128.0.13 puppet' >> /etc/hosts
 
 apt-get update
 apt-get install -y puppet-agent
+
+cat >> /etc/puppetlabs/puppet/puppet.conf <<EOF
+http_proxy_host = 10.142.0.43
+http_proxy_port = 3128
+no_proxy = localhost, 127.0.0.1, puppet
+EOF
+
+/opt/puppetlabs/bin/puppet agent -t
