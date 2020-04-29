4a21d1620cded6a40ea66f3d6e5cbeebff1d2488
Hector Vido <hector_vido@yahoo.com.br>
2020-04-29 11:49:01 -0300

    Commit 4

--- ansible/single-file/confs/database.sql
+++ ansible/single-file/confs/database.sql
@@ -1,5 +1,8 @@
 -- version 0.1
-CREATE DATABASE IF NOT EXISTS aplicacao;
+CREATE DATABASE IF NOT EXISTS `{{ DB_NAME }}`;
+CREATE USER IF NOT EXISTS '{{ DB_USER }}'@'%' IDENTIFIED BY '{{ DB_PASS }}';
+GRANT ALL ON `{{ DB_NAME }}`.* TO '{{ DB_USER }}'@'%';
+USE `{{ DB_NAME }}`;
 
 CREATE TABLE IF NOT EXISTS usuarios (
 	id INT PRIMARY KEY,
@@ -10,8 +13,5 @@ CREATE TABLE IF NOT EXISTS usuarios (
 );
 
 -- Ambas as senhas são um hash de 123
-@usuario = SELECT id FROM usuarios WHERE id = 1;
-SELECT IF(@usuario, INSERT INTO usuarios (nome, email, senha) VALUES (1, 'Paramahansa Yogananda', 'paramahansa@yogananda.in', '$2y$10$qTdhcJ8CkKztrvRhBN7EG.UB/YqfwjXpV2iKrZjvTIp2HTzqcflvi'), NULL);
-
-@usuario = SELECT id FROM usuarios WHERE id = 1;
-SELECT IF(@usuario, INSERT INTO usuarios (nome, email, senha) VALUES (2, 'Mary Shelley', 'victor@frankenstein.co.uk', '$2y$10$mKvUbxiLFx9V4WPcNT3dWehd9xJ5xyZi2wkmadK8UlJBnYrLpwAqi'), NULL);
+INSERT IGNORE INTO usuarios (id, nome, email, senha) VALUES (1, 'Paramahansa Yogananda', 'paramahansa@yogananda.in', '$2y$10$qTdhcJ8CkKztrvRhBN7EG.UB/YqfwjXpV2iKrZjvTIp2HTzqcflvi');
+INSERT IGNORE INTO usuarios (id, nome, email, senha) VALUES (2, 'Mary Shelley', 'victor@frankenstein.co.uk', '$2y$10$mKvUbxiLFx9V4WPcNT3dWehd9xJ5xyZi2wkmadK8UlJBnYrLpwAqi');
